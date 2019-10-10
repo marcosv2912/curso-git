72181170b1bcb6e9c022278ab650daa6e1d2aaad
Marcos Paixao <marcosv2912@gmail.com>
2019-10-10 09:47:48 -0300

    teste de alteração do git

--- Conversor/Program.cs
+++ Conversor/Program.cs
@@ -14,6 +14,8 @@ namespace Conversor
             Console.Write("Qual é a cotação do dólar: ");
             conv.cotacao = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
 
+            Console.WriteLine("Próximo passo: ");
+
             Console.Write("Quantos dólares voce vai comprar: ");
             conv.qtd = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
